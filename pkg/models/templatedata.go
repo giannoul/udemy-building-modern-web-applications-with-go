7b8b2dc7f2247ad385d063db2c79b53302edc6c3
giannoul <giannoul88@gmail.com>
2021-02-22 19:51:41 +0200

    Adding the mechanism to import data in the rendered templates

---
+++ pkg/models/templatedata.go
@@ -0,0 +1,13 @@
+package models
+
+// TemplateData is used in order to pass data to the page handlers
+type TemplateData struct {
+	StringMap map[string]string
+	IntMap    map[string]int
+	FloatMap  map[string]float32
+	Data      map[string]interface{}
+	CSRFToken string
+	Flash     string
+	Warning   string
+	Error     string
+}
